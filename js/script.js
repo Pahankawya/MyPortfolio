214437d0bd382896fcdd57aca0c727b9153e208c
Pahankawya <pahant24@gmail.com>
2023-05-17 03:56:12 +0530

    new animations

--- js/script.js
+++ js/script.js
@@ -27,3 +27,23 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.article-text', {delay:200, origin: 'top'});
+sr.reveal('.main-image', {delay:450, origin: 'top'});
+sr.reveal('.icons', {delay:500, origin: 'left'});
