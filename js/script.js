867f787a0279beebd7b20ff720c0ed6a693671bd
Pahankawya <pahant24@gmail.com>
2023-05-18 05:55:58 +0530

    new animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// assignment
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
@@ -28,6 +29,7 @@ var swiper = new Swiper(".slide-content", {
     },
   });
 
+// scroll
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
@@ -47,3 +49,27 @@ const sr = ScrollReveal({
 sr.reveal('.article-text', {delay:200, origin: 'top'});
 sr.reveal('.main-image', {delay:450, origin: 'top'});
 sr.reveal('.icons', {delay:500, origin: 'left'});
+
+// // scroll section
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+//
+// window.onscroll = () => {
+//     sections.forEach(sec =>{
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 100;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id')
+//
+//         if (top >= offset && top < offset + height){
+//             // active navbar links
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+//             })
+//         }
+//     })
+//     // sticky header
+//     let header = document.querySelector('header');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+// }
